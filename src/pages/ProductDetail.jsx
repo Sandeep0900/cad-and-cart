91bb75d508de54819988dd738ff43c4869403227
Sandeep0900 <sandeesharma09@gmail.com>
2025-06-17 02:21:53 +0530

    Add-to-Card was not working Made it Functional on product detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductById } from '../services/api';
+import { useCart } from '../context/CartContext.jsx';
+
 
 function ProductDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const { addToCart } = useCart();
+
+
 
   useEffect(() => {
     getProductById(id).then(res => setProduct(res.data));
@@ -13,17 +18,37 @@ function ProductDetail() {
   if (!product) return <p className="p-6">Loading...</p>;
 
   return (
-    <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-      <img src={product.thumbnail} alt={product.title} className="rounded w-full max-h-[400px] object-cover" />
+     <div className="max-w-6xl mx-auto p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-start">
+        {/* Image Section */}
+        <div className="bg-white shadow rounded overflow-hidden">
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="w-full h-full object-cover rounded"
+          />
+        </div>
 
-      <div>
-        <h1 className="text-3xl font-bold mb-2">{product.title}</h1>
-        <p className="text-gray-600 mb-4">{product.description}</p>
-        <p className="text-2xl text-green-600 font-semibold mb-2">${product.price}</p>
-        <p className="text-sm text-yellow-600 mb-4">Rating: {product.rating} ⭐</p>
-        <p className={`text-sm font-semibold ${product.stock > 0 ? 'text-green-500' : 'text-red-500'}`}>
-          {product.stock > 0 ? 'In Stock' : 'Out of Stock'}
-        </p>
+        {/* Details Section */}
+        <div>
+          <h1 className="text-3xl font-bold mb-3">{product.title}</h1>
+          <p className="text-gray-600 mb-4">{product.description}</p>
+
+          <div className="text-2xl font-semibold text-green-600 mb-2">
+            ${product.price}
+          </div>
+
+          <div className="mb-4">
+            <span className="text-yellow-500 font-semibold">
+              ⭐ {product.rating}
+            </span>
+            <span className="ml-2 text-gray-500">| Stock: {product.stock}</span>
+          </div>
+
+          <button onClick={()=> addToCart(product)} className="mt-4 bg-blue-600 text-white px-5 py-2 rounded hover:bg-blue-700 transition">
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   );
